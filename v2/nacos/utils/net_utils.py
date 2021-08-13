3a2866f6538b2501cbeab2226bebdcf929f0faa1
chenhongjing <60866283+chenhongjing@users.noreply.github.com>
2021-08-13 22:57:41 +0800

    add module utils #todo

---
+++ v2/nacos/utils/net_utils.py
@@ -0,0 +1,10 @@
+class NetUtils:
+    CLIENT_NAMING_LOCAL_IP_PROPERTY = "com.alibaba.nacos.client.naming.local.ip"
+    LEGAL_LOCAL_IP_PROPERTY = "java.net.preferIPv6Addresses"
+    DEFAULT_SOLVE_FAILED_RETURN = "resolve_failed"
+
+    def get_local_ip(self):
+        pass
+
+    def find_first_non_loop_back_address(self):
+        pass
---
+++ v2/nacos/utils/validator_utils.py
@@ -0,0 +1,10 @@
+class ValidatorUtils:
+    CONTEXT_PATH_MATCH = "(\\/)\\1+"
+
+    @staticmethod
+    def check_init_param(properties):
+        pass
+
+    @staticmethod
+    def check_context_path(context_path):
+        pass
