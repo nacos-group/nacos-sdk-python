3b4c37e753a43917d832ef84322728dbc5b07c5a
chenhongjing <60866283+chenhongjing@users.noreply.github.com>
2021-08-13 23:13:50 +0800

    add module security #todo

---
+++ v2/nacos/security/security_proxy.py
@@ -0,0 +1,70 @@
+import json
+import logging
+import urllib
+from typing import List
+from urllib.error import HTTPError, URLError
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+
+from v2.nacos.common.constants import Constants
+from v2.nacos.exception.nacos_exception import NacosException
+from v2.nacos.naming.utils.util_and_coms import UtilAndComs
+from v2.nacos.property_key_constants import PropertyKeyConstants
+
+
+class SecurityProxy:
+    LOGIN_URL = "/v1/auth/users/login"
+
+    def __init__(self, properties: dict):
+        logging.basicConfig()
+        self.logger = logging.getLogger(__name__)
+        self.username = properties[PropertyKeyConstants.USERNAME] = ""
+        self.password = properties[PropertyKeyConstants.PASSWORD] = ""
+        context_path = properties[PropertyKeyConstants.CONTEXT_PATH].strip()
+        self.context_path = context_path if context_path[0] == "/" else "/" + context_path
+        self.access_token = ""
+        self.token_ttl = None
+        self.token_refresh_window = None
+
+    def login_servers(self, server: List[str]) -> bool:
+        # todo
+        pass
+
+    def login_server(self, server: str) -> bool:
+        params = {PropertyKeyConstants.USERNAME: self.username}
+        body_map = {PropertyKeyConstants.PASSWORD: self.password}
+        url = UtilAndComs.HTTP + server + self.context_path + SecurityProxy.LOGIN_URL
+
+        if Constants.HTTP_PREFIX in server:
+            url = server + self.context_path + SecurityProxy.LOGIN_URL
+
+        try:
+            req = Request(url=url, data=urlencode(params).encode('utf-8') if params else None, method="POST")
+            resp = urlopen(req)
+            resp_data = resp.read()
+            # todo ?
+            obj = json.loads(resp_data).decode('utf-8')
+            if obj["code"] != 0 and obj["code"] != 200:
+                self.logger.error("Login fail: %s" % json.dumps(resp))
+                return False
+            message = eval(obj["message"])
+            if Constants.ACCESS_TOKEN in message.keys():
+                self.access_token = str(obj[message.ACCESS_TOKEN])
+                self.token_ttl = int(obj[message.TOKEN_TTL])
+                self.token_refresh_window = self.token_ttl/10
+        except URLError as e:
+            self.logger.error("[SecurityProxy] Login http request failed url: %s, params: %s, bodyMap: %s, errorMsg: %s"
+                              % (url, params, body_map, e))
+            return False
+        except NacosException as e:
+            self.logger.error("[SecurityProxy] Login http request failed url: %s, params: %s, bodyMap: %s, errorMsg: %s"
+                              % (url, params, body_map, e))
+            return False
+
+        return True
+
+    def get_access_token(self) -> str:
+        return self.access_token
+
+    def is_enabled(self) -> bool:
+        return self.username.strip() != ""
