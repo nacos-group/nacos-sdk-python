c75fe01de43f82f6342ea1471a3f6f693ddc5f92
chenhongjing <60866283+chenhongjing@users.noreply.github.com>
2021-08-13 22:55:34 +0800

    add module common

---
+++ v2/nacos/common/preserved_metadata_keys.py
@@ -0,0 +1,10 @@
+class PreservedMetadataKeys:
+    REGISTER_SOURCE = "preserved.register.source"
+
+    HEART_BEAT_TIMEOUT = "preserved.heart.beat.timeout"
+
+    IP_DELETE_TIMEOUT = "preserved.ip.delete.timeout"
+
+    HEART_BEAT_INTERVAL = "preserved.heart.beat.interval"
+
+    INSTANCE_ID_GENERATOR = "preserved.instance.id.generator"
