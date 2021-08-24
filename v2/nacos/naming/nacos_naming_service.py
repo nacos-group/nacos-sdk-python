55ea9244b8df9479a3f859c6c423e222ab517c7e
chenhongjing <60866283+chenhongjing@users.noreply.github.com>
2021-08-24 22:43:34 +0800

    modify nacos_naming_service.py on module naming

--- v2/nacos/naming/nacos_naming_service.py
+++ v2/nacos/naming/nacos_naming_service.py
@@ -2,13 +2,19 @@ import logging
 from typing import List
 
 from v2.nacos.naming.cache.service_info_holder import ServiceInfoHolder
+from v2.nacos.naming.core.balancer import Balancer
 from v2.nacos.naming.dtos.instance import Instance
 from v2.nacos.naming.event.instances_change_notifier import InstancesChangeNotifier
+from v2.nacos.naming.ievent_listener import EventListener
 from v2.nacos.naming.remote.naming_client_proxy_delegate import NamingClientProxyDelegate
 from v2.nacos.naming.utils.naming_utils import NamingUtils
 
 
 class NacosNamingService:
+    UP = "UP"
+
+    DOWN = "DOWN"
+
     def __init__(self, properties: dict):
         logging.basicConfig()
         self.logger = logging.getLogger(__name__)
@@ -25,32 +31,79 @@ class NacosNamingService:
         NamingUtils.check_instance_is_legal(instance)
         self.client_proxy.register_service(service_name, group_name, instance)
 
-    def deregister_instance(self, service_name: str, gourp_name: str, ip: str, port: int, cluster_name: str) -> None:
-        pass
+    def deregister_instance(self, service_name: str, group_name: str, ip: str, port: int, cluster_name: str) -> None:
+        instance = Instance(ip=ip, port=port, cluster_name=cluster_name, service_name=service_name)
+        self.client_proxy.deregister_service(service_name, group_name, instance)
 
     def get_all_instances(self, service_name: str, group_name: str, clusters: List[str], subscribe: bool) -> List[Instance]:
-        pass
+        cluster_string = ",".join(clusters)
+        if subscribe:
+            service_info = self.service_info_holder.get_service_info(service_name, group_name, cluster_string)
+        else:
+            service_info = self.client_proxy.query_instances_of_service(
+                service_name, group_name, cluster_string, 0, False
+            )
+        if not service_info or not service_info.get_hosts():
+            return []
+
+        return service_info.get_hosts()
 
     def select_instances(self, service_name: str, group_name: str, clusters: List[str], healthy: bool, subscribe: bool) -> List[Instance]:
-        pass
+        cluster_string = ",".join(clusters)
+        if subscribe:
+            service_info = self.service_info_holder.get_service_info(service_name, group_name, cluster_string)
+            if not service_info:
+                service_info = self.client_proxy.subscribe(service_name, group_name, cluster_string)
+        else:
+            service_info = self.client_proxy.query_instances_of_service(
+                service_name, group_name, cluster_string, 0, False
+            )
+
+        if not service_info or not service_info.get_hosts():
+            return []
+
+        instances_list = service_info.get_hosts()
+        for instance in instances_list:
+            if healthy != instance.is_healthy() or not instance.is_enabled() or instance.get_weight() <= 0:
+                instances_list.remove(instance)
+
+        service_info.set_hosts(instances_list)
+        return instances_list
 
     def select_one_healthy_instance(self, service_name: str, group_name: str, clusters: List[str], subscribe: bool) -> Instance:
-        pass
+        cluster_string = ",".join(clusters)
+        if subscribe:
+            service_info = self.service_info_holder.get_service_info(service_name, group_name, cluster_string)
+            if not service_info:
+                service_info = self.client_proxy.subscribe(service_name, group_name, cluster_string)
+            return Balancer.RandomByWeight.select_host(service_info)
+        else:
+            service_info = self.client_proxy.query_instances_of_service(
+                service_name, group_name, cluster_string, 0, False
+            )
+            return Balancer.RandomByWeight.select_host(service_info)
 
     def subscribe(self, service_name: str, group_name: str, clusters: List[str], listener: EventListener) -> None:
-        pass
+        if not listener:
+            return
+
+        cluster_string = ",".join(clusters)
+        self.change_notifier.register_listener(group_name, service_name, cluster_string, listener)
+        self.client_proxy.subscribe(service_name, group_name, cluster_string)
 
     def unsubscribe(self, service_name: str, group_name: str, clusters: List[str], listener: EventListener) -> None:
-        pass
+        cluster_string = ",".join(clusters)
+        self.change_notifier.deregister_listener(group_name, service_name, cluster_string, listener)
 
-    def get_services_of_server(self):
-        pass
+        if not self.change_notifier.is_subscribed(group_name, service_name, cluster_string):
+            self.client_proxy.unsubscribe(service_name, group_name, cluster_string)
+
+    def get_services_of_server(self, page_no: int, page_size: int, group_name: str, selector):
+        return self.client_proxy.get_service_list(page_no, page_size, group_name, selector)
 
     def get_server_status(self) -> str:
-        pass
+        return NacosNamingService.UP if self.client_proxy.server_healthy() else NacosNamingService.DOWN
 
     def shutdown(self) -> None:
-        pass
-
-
-
+        self.service_info_holder.shutdown()
+        self.client_proxy.shutdown()
