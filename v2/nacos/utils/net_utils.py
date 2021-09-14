fb4cf4f8eb11dbf6e63008000599996a969cece5
chenhongjing <60866283+chenhongjing@users.noreply.github.com>
2021-09-14 10:29:52 +0800

    add content on v2/nacos/utils

--- v2/nacos/utils/net_utils.py
+++ v2/nacos/utils/net_utils.py
@@ -1,11 +1,11 @@
+from v2.nacos.utils.env_util import EnvUtil
+
+system_properties = EnvUtil().get_system_properties()
+
+
 class NetUtils:
     CLIENT_NAMING_LOCAL_IP_PROPERTY = "com.alibaba.nacos.client.naming.local.ip"
-    LEGAL_LOCAL_IP_PROPERTY = "java.net.preferIPv6Addresses"
-    DEFAULT_SOLVE_FAILED_RETURN = "resolve_failed"
 
-    def get_local_ip(self):
-        # todo
-        return "192.168.220.22"
-
-    def find_first_non_loop_back_address(self):
-        pass
+    @staticmethod
+    def get_local_ip():
+        return system_properties.get(NetUtils.CLIENT_NAMING_LOCAL_IP_PROPERTY)
--- v2/nacos/utils/validator_utils.py
+++ v2/nacos/utils/validator_utils.py
@@ -1,10 +1,20 @@
+import re
+
+from v2.nacos.exception.nacos_exception import NacosException
+from v2.nacos.property_key_constants import PropertyKeyConstants
+
+
 class ValidatorUtils:
     CONTEXT_PATH_MATCH = "(\\/)\\1+"
 
     @staticmethod
-    def check_init_param(properties):
-        pass
+    def check_init_param(properties: dict):
+        ValidatorUtils.check_context_path(properties.get(PropertyKeyConstants.CONTEXT_PATH))
 
     @staticmethod
     def check_context_path(context_path):
-        pass
+        if not context_path:
+            return
+
+        if re.search(ValidatorUtils.CONTEXT_PATH_MATCH, context_path):
+            raise NacosException("Illegal url path expression")
