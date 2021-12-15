7bb544b3cff37c0c563a9a473166402cf3f8a32e
ChunLin Diao <chunlin.diao@dxc.com>
2021-12-15 21:45:29 +0800

    1.set user_home with current location of the home directory for cross-platform 2.get rid of the defautl value for JM_SNAPSHOT_PATH, if not user_home logic will never be invoked

--- v2/nacos/utils/arg_util.py
+++ v2/nacos/utils/arg_util.py
@@ -1,6 +1,6 @@
 import json
 from argparse import ArgumentParser
-
+from os.path import expanduser
 
 # class ArgUtil:
 #     ARGS_PATH = "commandline_args.json"
@@ -18,8 +18,8 @@ from argparse import ArgumentParser
 # todo add more arguments and annotations
 
 arg_parser = ArgumentParser(description="Nacos Python SDK")
-arg_parser.add_argument("--user_home", "-u", type=str, default="/Users/sunli")
+arg_parser.add_argument("--user_home", "-u", type=str, default=expanduser("~"))
 arg_parser.add_argument("--com_alibaba_nacos_client_naming_local_ip", "-i", type=str, default="127.0.0.1")
 arg_parser.add_argument("--line_separator", "-l", type=str, default=";")
-arg_parser.add_argument("--JM_SNAPSHOT_PATH", "-j", type=str, default="/Users/sunli/nacos/data")
+arg_parser.add_argument("--JM_SNAPSHOT_PATH", "-j", type=str)
 
